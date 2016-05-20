ab0d6338e49d23c3f5a116ee414531691deed23b
Noe <noeon270@hotmail.com>
2016-05-20 15:54:35 -0500

    add symbol pics, change px to vw, add mobile site notice, add animations

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,18 @@
+$(document).on('ready page:load', ready);
+function ready() {
+
+  $( "#part1" ).animate({
+    opacity: 1,
+    height:250,
+  }, 1000 );
+
+  $( "#part2" ).animate({
+    opacity: 1
+    // fontSize: "1.5em",
+  }, 2000 );
+
+  $( "#part3" ).animate({
+    opacity: 1
+  }, 2500 );
+
+}
